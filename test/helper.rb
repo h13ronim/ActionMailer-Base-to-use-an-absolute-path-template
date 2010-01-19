b5a06839672a66278d06202a7801b961a72b43bc
Marcin Michalowski <h13ronim@gmail.com>
2010-01-19 14:01:25 +0100

    Initial commit to ActionMailer-Base-to-use-an-absolute-path-template.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'ActionMailer-Base-to-use-an-absolute-path-template'
+
+class Test::Unit::TestCase
+end
